ef485e704b7871133a14c09f78316550222f8e8e
Léo Godard <leo.godard@epitech.eu>
2021-10-01 17:10:18 +0200

    Add DB connection

--- api/tasks-get/index.js
+++ api/tasks-get/index.js
@@ -1,20 +1,18 @@
+const mongoClient = require("mongodb").MongoClient;
+
 module.exports = async function(context, req) {
-    const tasks = [{
-            id: 1,
-            label: "🍔 Eat",
-            status: ""
-        },
-        {
-            id: 2,
-            label: "🛏 Sleep",
-            status: ""
-        },
-        {
-            id: 3,
-            label: "</> Code",
-            status: ""
-        }
-    ];
+    const header = req.headers['x-ms-client-principal'];
+    const encoded = Buffer.from(header, 'base64');
+    const decoded = encoded.toString('ascii');
+    const user = JSON.parse(decoded);
+    const client = await mongoClient.connect("mongodb://beautiful-dbname:NH5dveNPyMaPXz6z0BkLcuxZJWAXpQbcLxphG82ywobOXTQ3MgX28qZrA1OWWAo9hWoCPUQRZgHBncumxesvkg%3D%3D@beautiful-dbname.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@beautiful-dbname@");
+    const database = client.db("tasks");
+
+    const response = await database.collection("tasks").find({
+        userId: user.userId
+    });
+
+    const tasks = await response.toArray();
 
     context.res = {
         // status: 200, /* Defaults to 200 */
